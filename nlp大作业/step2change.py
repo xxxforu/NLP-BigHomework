ac94ff0ea9d793c4d23cfb363b803fa3f419d5a9
刘星宇 <3231409326@qq.com>
2024-12-09 19:45:27 +0800

    step1-3

---
+++ nlp大作业/step2change.py
@@ -0,0 +1,38 @@
+import re
+import opencc
+
+# 初始化opencc转换器
+
+cc = opencc.OpenCC(r"C:\Users\32314\anaconda3\envs\pytorch\Lib\site-packages\opencc\config\t2s")  # 使用正确的路径
+
+def preprocess_text(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    processed_lines = set()  # 用set去除重复的行
+    cleaned_lines = []
+
+    for line in lines:
+        # 移除空行
+        if not line.strip():
+            continue
+
+        # 提取中文部分，删除中文后面的多余内容
+        match = re.match(r'([^\u4e00-\u9fa5]+)([\u4e00-\u9fa5]+)', line.strip())
+        if match:
+            # 提取的中文部分转换为简体
+            simplified_chinese = cc.convert(match.group(2).strip())
+            cleaned_line = match.group(1).strip() + '\t' + simplified_chinese
+            # 将处理后的行加入set中去重
+            processed_lines.add(cleaned_line)
+
+    # 将去重后的内容写入输出文件
+    with open(output_file, 'w', encoding='utf-8') as f:
+        for line in processed_lines:
+            f.write(line + '\n')
+
+
+# 使用示例
+input_file = 'clean1.txt'  # 输入文件名
+output_file = 'clean2.txt'  # 输出文件名
+preprocess_text(input_file, output_file)
